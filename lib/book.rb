e6bc040745e16dbc3966b2ead79b6ca40fc1bf02
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-14 03:24:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,7 @@
 # book.rb
-
 class Book
-  attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   GENRES = []
 
@@ -14,9 +13,10 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-  # create the writer for genre and add the logic for the class constant
   def genre=(genre)
     @genre = genre
     GENRES << genre
+    #or GENRES = @genre
   end
+
 end
